30180fcca9c912d7e4f46637a4a85d0f820472d2
Asorr <hello.git.moviemaker251@gmail.com>
2026-06-10 03:28:35 +0900

    탭메뉴 UI(레이아웃) 1. 여행목록 2. 앱정보/지도

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -1,20 +1,135 @@
 package com.example.final_project
 
+import android.content.Context
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
+import android.widget.Button
+
+import android.widget.DatePicker
+
+import android.widget.EditText
+
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import java.io.IOException
+
+import java.util.Calendar
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    lateinit var datePicker1: DatePicker
+
+    lateinit var edtDiary: EditText
+
+    lateinit var btnWrite: Button
+
+    lateinit var fileName: String
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        datePicker1 = findViewById(R.id.datePicker1)
+
+        edtDiary = findViewById(R.id.edit)
+
+        btnWrite = findViewById(R.id.btnWrite)
+
+
+
+        val cal = Calendar.getInstance()
+
+        val y = cal.get(Calendar.YEAR)
+
+        val m = cal.get(Calendar.MONTH)
+
+        val d = cal.get(Calendar.DAY_OF_MONTH)
+
+
+
+        fileName = "${y}_${m + 1}_$d.txt"
+
+        displayDiary(fileName)
+
+
+
+        datePicker1.init(y, m, d) { _, year, monthOfYear, dayOfMonth ->
+
+            fileName = "${y}_${m + 1}_$d.txt"
+
+            displayDiary(fileName)
+
         }
+
+
+
+        btnWrite.setOnClickListener { saveDiary() }
+
     }
+
+
+
+    private fun displayDiary(name: String) {
+
+        val diText = readDiary(name)
+
+        edtDiary.setText(diText)
+
+        btnWrite.text = if (diText != null) "수정하기"
+
+        else "새로 저장"
+
+        edtDiary.hint = diText ?: "일기 없음"
+
+
+
+        btnWrite.isEnabled = true
+
+    }
+
+
+
+// 여기 작성
+    private fun saveDiary() {
+        try {
+
+            openFileOutput(fileName, Context.MODE_PRIVATE).use { it.write(edtDiary.text.toString().toByteArray())}
+
+            Toast.makeText(this, "$fileName 이 저장됨", Toast.LENGTH_SHORT).show()
+
+        } catch (e: IOException) {
+
+            Toast.makeText(this, "파일 저장에 실패했습니다:  ${e.message}", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+    private fun readDiary(fName: String): String? {
+
+        return try {
+
+            openFileInput(fName).bufferedReader().use { it.readText().trim() }
+
+        } catch (e: IOException) {
+
+            null
+
+        }
+
+    }
+
 }
